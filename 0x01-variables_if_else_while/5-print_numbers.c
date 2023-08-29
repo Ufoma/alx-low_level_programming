57b9e4cd52f7e5ff597a696790531b02b25b86d9
Ufoma <dymondintheski@gmail.com>
2023-08-28 20:32:56 -0700

    correction

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print the numbers '0' to '9'.
+ *
+ * Return: null
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
+
