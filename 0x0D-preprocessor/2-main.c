b3c92763a6c5c0a2a549bc2ea81c5a9a7769b53b
Ufoma <dymondintheski@gmail.com>
2023-09-11 12:59:57 -0700

    Diamond is preprocessing conditional inclusion

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - point of entry
+ *
+ * Description: This is program that prints the name of the file
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,4 @@
+Project: First Preprocessor work in C programming language
+How to compile using C
+How the preprocessor works.
+No optional tasks 
