baf28d581f8d0c1c21b7ae4b924dfc829a99d14d
Ufoma <dymondintheski@gmail.com>
2023-12-01 21:56:05 -0800

    deleted some files

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -35,37 +35,4 @@ int main(int argc, char *argv[])
 {
 	int fd;
 	ssize_t r;
-	Elf64_Ehdr *helf;
-
-	if (argc != 2)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s <elf_file>\n", argv[0]);
-		exit(98);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: could not open %s\n", argv[1]);
-		exit(98);
-	}
-	helf = malloc(sizeof(Elf64_Ehdr));
-	if (helf == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: out of memory\n");
-		close(fd);
-		exit(98);
-	}
-	r = read(fd, helf, sizeof(Elf64_Ehdr));
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: could not read %s\n", argv[1]);
-		free(helf);
-		close(fd);
-		exit(98);
-	}
-	close(fd); /* Close the file descriptor after reading the ELF header */
-	get_magic(helf, argv[1], fd);
-	/*  Call other functions for further analysis */
-	free(helf);
-	exit(EXIT_SUCCESS);
-}
+	Elf64_Ehdr *helf
